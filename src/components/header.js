a9ea30b46f501904a3ac3a61c134cb08c271509e
Alexis Ortiz Ojeda <aortizoj15@gmail.com>
2021-05-10 01:30:17 -0700

    replaceed about bg, and applied custom colors

--- src/components/header.js
+++ src/components/header.js
@@ -3,7 +3,7 @@ import PropTypes from "prop-types"
 import { Link } from "gatsby"
 
 const Header = ({ siteTitle }) => (
-  <header className="bg-blue-700 px-1 py-0.5">
+  <header className="bg-blue px-1 py-0.5">
     <div className="my-0 mx-auto max-w-xl lg:max-w-4xl">
       <h1 className="text-base md:text-lg m-0 text-white text-center md:text-left">
         <Link to="/">{siteTitle}</Link>
