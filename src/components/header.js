24e1fb6621b9d434178c2e226d80a7863e7ed6dc
Alexis Ortiz Ojeda <aortizoj15@gmail.com>
2021-05-09 23:58:12 -0700

    Added header

--- src/components/header.js
+++ src/components/header.js
@@ -3,29 +3,10 @@ import PropTypes from "prop-types"
 import { Link } from "gatsby"
 
 const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
+  <header className="bg-blue-700 px-1 py-0.5">
+    <div className="my-0 mx-auto max-w-xl lg:max-w-4xl">
+      <h1 className="text-base md:text-lg m-0 text-white text-center md:text-left">
+        <Link to="/">{siteTitle}</Link>
       </h1>
     </div>
   </header>
